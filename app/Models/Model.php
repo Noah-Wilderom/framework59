2d26629cf9a0a4e569e9ab6123a9fbf7f8acee61
Noah Wilderom <61543656+Noah-Wilderom@users.noreply.github.com>
2022-04-20 16:14:52 +0200

    Update Model.php

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -1,7 +1,30 @@
 <?php
 namespace Framework59;
 
+use Framework59\Database;
+
 class Model {
+    protected static $conn;
+
+    public function __construct() {
+        self::$conn = new Database;
+    }
+
+    public static function where() {
+        return self::$conn->query('SELECT * FROM ' . $this->table . 'WHERE')
+    }
+
+    public static function get() {
+        return self::$conn->resultSet();
+    }
+
+    public static function all() {
+
+    }
+
+    public static function where() {
+
+    }
 
     
 }
